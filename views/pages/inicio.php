8f7c55444581f961dbaa31d107f9630ced48e33d
Gerardo Vega <54243381+Gerardo703@users.noreply.github.com>
2021-11-02 18:25:57 -0300

    Modified views

--- views/pages/inicio.php
+++ views/pages/inicio.php
@@ -1,5 +1,5 @@
-<div class="p-5 bg-dark text-white mt-3">
-    <div class="container">
+<div class="p-5 bg-dark text-white mt-3 rounded">
+    <div class="container ">
         <h1 class="display-3">Bienvenido</h1>
         <p class="lead">CRUD PHP</p>
         <hr class="my-2">

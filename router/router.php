b49e0f347b5162bfe142c4a8b81b0984486694b8
Gerardo Vega <54243381+Gerardo703@users.noreply.github.com>
2021-11-08 00:32:15 -0300

    *

--- router/router.php
+++ router/router.php
@@ -3,9 +3,9 @@
     // echo $controller;
     // echo $action;
 
-    // Archivo que maneja la rutas
+    // Archivo que maneja la rutas de manera dinámica
     include_once("controllers/controller_".$controller.".php");
-    $objController = "Controller".ucfirst($controller);
+    $objController = "Controller".ucfirst($controller); // Se arma el nombre del controlador
     $controller = new $objController();
     $controller->$action();
 
